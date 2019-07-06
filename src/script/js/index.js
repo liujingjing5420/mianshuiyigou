a18c28a65c2fd7e8a5b66938b28cf81e8b1b5f94
liujingjing5420 <1490104275@qq.com>
2019-07-06 18:03:45 +0800

    添加滚动条事件

--- src/script/js/index.js
+++ src/script/js/index.js
@@ -44,5 +44,13 @@
 
 !function () {
 	//小效果
-
+	// 楼梯效果
+	const $louti = $('.floor-nav-left');
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() >= 400) {
+			$louti.css('display', 'block');
+		} else {
+			$louti.css('display', 'none');
+		}
+	})
 }(jQuery);
