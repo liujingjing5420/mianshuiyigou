8f7a7165a467499906eba263d024e257da6008c9
liujingjing5420 <1490104275@qq.com>
2019-07-09 21:12:42 +0800

    购物车基本完成提交

--- src/script/js/cart.js
+++ src/script/js/cart.js
@@ -1,87 +1,110 @@
 ; !function ($) {
-    let arrid = $.cookie('goodsid').split(',');
-    let arrnums = $.cookie('goodsnums').split(',');
-    let htmlstr = '';
-    console.log(arrid);
-    console.log(arrnums);
-    $.ajax({
-        url: 'http://10.31.158.39:8088/javascript/mianshuiyigou/php/piclist.php',
-        dataType: 'json',
-    }).done(function (data) {
-        console.log(data);
-        $.each(arrid, function (index1, value1) {
-            $.each(data, function (index2, value2) {
-                if (value1 === value2.picid) {
-                    htmlstr += `
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td class="td30"><input class="checkone" type="checkbox" goods_id="44660" checked="checked" /></td>
-                                <td class="td343">
-                                    <dl>
-                                        <dd>
-                                            <img src="${value2.url}"
-                                                alt="${value2.des}" style="width:105px;height:105px"/>
-                                        </dd>
-                                        <dt style="cursor:pointer"
-                                            onclick="javascript:window.open('https://item.mianshui365.com/goods__c809280112706.html')">
-                                            <p class="color1">${value2.title}</p>
-                                            <p class="color2">${value2.des}</p>
-                                        </dt>
-                                    </dl>
-                                </td>
-                                <td class="td83">
-                                </td>
-                                <td class="td159" style="position: relative">
-                                    <p class="color11">免税价：<strong>&yen;${value2.price}</strong></p>
-                                    <div class="color11 change-activity">
-                                        修改促销&gt;
-                                    </div>
-                                    <div class="change-panel">
-                                        <div>
-                                            <input type="radio" style="vertical-align: top" name="activityId_44660" goodsid="44660"
-                                                activity_code="AI2019070117013205200" checked="" />
-                                            <div style="display: inline-block;width: 90%">
-                                                每满1000减140&gt;
-                                            </div>
-                                        </div>
-                                        <div>
-                                            <input type="radio" style="vertical-align: top" name="activityId_44660" goodsid="44660"
-                                                activity_code="-1" />
-                                            <div style="display: inline-block;width: 90%">
-                                                不使用优惠活动
-                                            </div>
-                                        </div>
-                                        <p style="text-align: center;padding: 10px;"><span class="cp_sub">确认</span><span
-                                                class="cp_close" style="background: #999999;margin-left: 50px">取消</span> </p>
-                                    </div>
-                                    <div class="yzbottom-writebg"></div>
-                                </td>
-                                <td class="td138">
-                                    <p class="jiajia"> 
-                                    <a href="javascript:;" class="reduce " id="item-reduce-44660"
-                                            goods_id="44660">-</a> 
-                                            <input class="val" count="${arrnums[index1]}" value="${arrnums[index1]}" id="item-change-44660"
-                                            goods_id="44660" goods_limit="" />
-                                             <a href="javascript:;" class="add" id="item-plus-44660" goods_id="44660">+</a> </p>
-                                    <p> </p>
-                                </td>
-                                <td class="td169">
-                                    <p><a href="javascript:;" onclick="javascript:goodsCollect(44660,this);" class="lajix">加入收藏</a>
-                                    </p>
-                                    <p><a href="javascript:;"
-                                            onclick="javascript:hmpub.confirm_alert('您确定要从购物车中删除此商品吗?', 'https://www.mianshui365.com/index.php?_a=clearItem&amp;_c=cart&amp;backUrl=%3F_c%3Dcart%26_a%3Dindex&amp;skuid=44660');"
-                                            class="sj">删除商品</a></p>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    `;
-                }
+    //获取cookie中所存的购物车商品的信息，渲染。
+    if ($.cookie('goodsid') && $.cookie('goodsnums')) {
+        let arrid = $.cookie('goodsid').split(',');
+        let arrnums = $.cookie('goodsnums').split(',');
+        let htmlstr = '';
+        $.ajax({
+            url: 'http://10.31.158.39:8088/javascript/mianshuiyigou/php/piclist.php',
+            dataType: 'json',
+        }).done(function (data) {
+            $.each(arrid, function (index1, value1) {
+                $.each(data, function (index2, value2) {
+                    if (value1 === value2.picid) {
+                        const $cloneTable = $('.needed').first().clone(true);
+                        $cloneTable.css('display', 'block');
+                        $('.cuxiao1').append($cloneTable);
+                        $cloneTable.find('.td343 dl dd img').attr('src', value2.url + '?picid=' + value2.picid);
+                        $cloneTable.find('.td343 dl dd alt').attr('alt', value2.des);
+                        $cloneTable.find('.color1').html(value2.title);
+                        $cloneTable.find('.color2').html(value2.des);
+                        $cloneTable.find('.color11 strong').html(value2.price);
+                        $cloneTable.find('.jiajia input').attr('value', arrnums[index1]);
+
+                    }
+                })
+            })
+        });
+        // 购物车页面商品数量的变化
+        $('.cuxiao1').on('click', '.add', function () {
+            let num = $(this).prev().val();
+            num++;
+            $(this).prev().val(num);
+            // 将改变后商品的数量存入cookie中
+            let picid = $(this).parents('tr').find('.td343 dl dd img').attr('src').split('?')[1].slice(6);
+            let index = $.inArray(picid, arrid);
+            if (arrnums[index] !== $(this).prev().val()) {
+                arrnums[index] = $(this).prev().val();
+                $.cookie('goodsnums', arrnums.toString());
+            }
+        })
+        $('.cuxiao1').on('click', '.reduce', function () {
+            let num = $(this).next().val();
+            num--;
+            if (num <= 1) {
+                num = 1;
+            }
+            $(this).next().val(num);
+            // 将改变后商品的数量存入cookie中
+            let picid = $(this).parents('tr').find('.td343 dl dd img').attr('src').split('?')[1].slice(6);
+            let index = $.inArray(picid, arrid);
+            if (arrnums[index] !== $(this).next().val()) {
+                arrnums[index] = $(this).next().val();
+                $.cookie('goodsnums', arrnums.toString());
+            }
+        })
+
+
+        // 删除商品
+        $('.cuxiao1').on('click', '.sj', function () {
+            console.log(this);
+            const $parents = $(this).parents('table');
+            // 将删除的商品从cookie中删掉
+            let picid = $parents.find('.td343 dl dd img').attr('src').split('?')[1].slice(6);
+            let index = $.inArray(picid, arrid);
+            arrid.splice(index, 1);
+            arrnums.splice(index, 1);
+            $.cookie('goodsid', arrid.toString());
+            $.cookie('goodsnums', arrnums.toString());
+            $parents.css({
+                display: 'none'
             })
         })
+
+    } else {
+        let htmlstr = '';
+        htmlstr += `<div class="empty">您的购物车什么都没有，快去<a href="index111.html">购物</a>吧~</div>`;
         $('.cuxiao1').html(htmlstr);
-    });
+        $('.block-deliver').css('display', 'none');
+        $('.tiaomu').css('display', 'none');
+        $('#bottomdiv-wrap').css('display', 'none');
+    }
+
+    // 全选按钮的实现
+    // 全选按钮
+    const $checkall = $('.checkall');
+    const $checkone = $('.checkone');
+    $checkall.on('click', function () {
+        if ($checkall.attr('checked')) {
+            $checkall.removeAttr('checked');
+            // $checkone.each(function (index, value) {
+            //     $(this).removeAttr('checked');
+            // })
+            console.log($checkone);
+            console.log($checkone.attr('checked'));
+            // $checkone.removeAttr('checked');
+            $checkone.each(function (index, value) {
+                // $(this).css('display', 'none');
+                alert(value);
+            })
+        } else {
+            $checkall.attr('checked');
+            $checkone.each(function (index, value) {
+                $(this).attr('checked', false);
+            })
+        }
+    })
+
 
 
 }(jQuery);
